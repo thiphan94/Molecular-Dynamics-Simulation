a8b057bcf8702aca7e23e15f25fbfa1d913759db
Thi Phan <thiphan94@gmail.com>
2021-06-25 11:11:28 +0200

    count frame

---
+++ ALA3/molecular_dynamics_simulation.py
@@ -0,0 +1,40 @@
+"""Calcultation of atomic distances."""
+
+import math
+
+
+"""FUnction to open the file."""
+
+
+def open_file(name_file):
+    try:
+        f = open(name_file, "r")
+        return f
+    except IOError:
+        print("Error: File does not appear to exist !")
+        return 0
+
+
+"""Function to count frames in file."""
+
+
+def count_frame(file):
+    count = 0
+    for line in file:
+        # line starts with "G"
+        if line.startswith("G"):
+            count += 1
+    return count
+
+
+"""Main function."""
+
+
+def Molecular_Dynamics_Simulation(file_input):
+    file_opened = open_file(file_input)
+    number_frames = count_frame(file_opened)
+    print("Number of frames in file:", number_frames)
+
+
+file_input = "data.gro"
+Molecular_Dynamics_Simulation(file_input)

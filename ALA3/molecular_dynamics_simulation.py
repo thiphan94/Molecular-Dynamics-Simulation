4c1382ce0ccd88fbe9e63a620b661712fc489e35
Thi Phan <thiphan94@gmail.com>
2021-07-20 15:26:05 +0200

    matrix

--- ALA3/molecular_dynamics_simulation.py
+++ ALA3/molecular_dynamics_simulation.py
@@ -47,6 +47,7 @@ def count_distance(file, number, from_index, to_index):
             pattern = "=(.+?)step"
             if line == number:
                 t = re.search("=(.+?)step", lines[index - 1]).group(1)
+
                 atom1 = lines[index + 1]
                 atom2 = lines[index + int(number)]
                 list_atom1 = atom1.split()
@@ -59,7 +60,7 @@ def count_distance(file, number, from_index, to_index):
                     + ((list_atom2[2] - list_atom1[2]) ** 2)
                 ) ** 0.5
 
-        return t, distance
+        return float(t), distance
 
 
 def count_angle(file, number):
@@ -103,75 +104,59 @@ def count_angle(file, number):
                     list_angle_psi.append(psi[:])
                     del psi[:3]
 
-        # print(list_angle_phi)
-        # print(list_angle_psi)
         return (list_angle_psi, list_angle_phi)
 
 
 """Function to calculate dihedral angles."""
 
 
-def dihedral_angle(file, number):
+def dihedral_angle(file, number, list_angle_psi, list_angle_phi, from_index, to_index):
     with open(file) as f:
-        last = int(number) + 2
+        # last = int(number) + 2
         lines = f.readlines()
-        # list of atoms to calculate angle phi
-        list_angle_phi = []
-        # list of atoms to calculate angle psi
-        list_angle_psi = []
-        # list of coordinates_atoms to calculate angle phi
-        list_coordinates_phi = []
-        # list of coordinates_atoms to calculate angle psi
+        lines = lines[from_index:to_index]
+
         list_coordinates_psi = []
-        # index of four atoms in chain phi
-        phi = []
-        # index of four atoms in chain psi
-        psi = []
+        list_coordinates_phi = []
         phi_coordinates = []
         psi_coordinates = []
         vector_psi = []
         vector_phi = []
-        for index, line in enumerate(lines[0:last]):
+
+        for index, line in enumerate(lines):
             if len(lines[index].split()) > 4 and lines[index].split()[1] != "by":
-                if lines[index].split()[1] == "N":
-                    psi.append(lines[index].split()[2])
+                if any(
+                    lines[index].split()[2] in sublist for sublist in list_angle_psi
+                ):
+
+                    # print(lines[index].split()[2])
                     psi_coordinates.append(lines[index].split()[3:6])
-                    if len(phi) == 1:
-                        phi.append(lines[index].split()[2])
-                        phi_coordinates.append(lines[index].split()[3:6])
-                if lines[index].split()[1] == "CA":
-                    psi.append(lines[index].split()[2])
-                    psi_coordinates.append(lines[index].split()[3:6])
-                    if len(phi) == 2:
-                        phi.append(lines[index].split()[2])
-                        phi_coordinates.append(lines[index].split()[3:6])
-                if lines[index].split()[1] == "C":
-                    psi.append(lines[index].split()[2])
-                    psi_coordinates.append(lines[index].split()[3:6])
-                    if len(phi) == 0 or len(psi) == 3:
-                        phi.append(lines[index].split()[2])
-                        phi_coordinates.append(lines[index].split()[3:6])
-                # print("psi:", psi)
-                if len(phi) == 4:
-                    list_angle_phi.append(phi[:])
-                    del phi[:3]
-                if len(psi) == 4:
-                    list_angle_psi.append(psi[:])
-                    del psi[:3]
+
+                if any(
+                    lines[index].split()[2] in sublist for sublist in list_angle_phi
+                ):
+
+                    # print(lines[index].split()[2])
+                    phi_coordinates.append(lines[index].split()[3:6])
+
                 if len(phi_coordinates) == 4:
                     list_coordinates_phi.append(phi_coordinates[:])
                     del phi_coordinates[:3]
+
                 if len(psi_coordinates) == 4:
                     list_coordinates_psi.append(psi_coordinates[:])
                     del psi_coordinates[:3]
 
         # print(list_coordinates_psi)
-        # print(list_coordinates_phi)
-        # vector_psi, vector_phi = vector(list_coordinates_psi, list_coordinates_phi)
         #
-        # arccos_angle(vector_psi, vector_phi)
+        # print(list_coordinates_phi)
+        vector_psi, vector_phi = vector(list_coordinates_psi, list_coordinates_phi)
+        #
+        angle_psi, angle_phi = value_angle(vector_psi, vector_phi)
 
-        print(list_angle_phi)
+        return angle_psi, angle_phi
+
+        # print(list_angle_phi)
 
 
 """Function to calculate vector between 4 atoms."""
@@ -239,7 +224,7 @@ def vector(list_coordinates_psi, list_coordinates_phi):
         del ij_phi[:]
         del kj_phi[:]
         del kl_phi[:]
-
+    #
     # print(vector_psi)
     # print(vector_phi)
     return vector_psi, vector_phi
@@ -248,34 +233,55 @@ def vector(list_coordinates_psi, list_coordinates_phi):
 """Function to calculate vector im and vector ln."""
 
 
-def arccos_angle(vector_psi, vector_phi):
-
+def value_angle(vector_psi, vector_phi):
+    list_angle_psi = []
+    list_angle_phi = []
     for vector in vector_psi:
         vector_ij = np.array(vector[0])
         vector_kj = np.array(vector[1])
         vector_kl = np.array(vector[2])
-        print(vector_ij)
-        print(vector_kj)
-        print(vector_kl)
 
         im = vector_ij - np.dot(
             (np.dot(vector_ij, vector_kj) / np.linalg.norm(vector_kj)), vector_kj
         )
-        print("im =", im)
 
         ln = -vector_kl + np.dot(
             (np.dot(vector_kl, vector_kj) / np.linalg.norm(vector_kj)), vector_kj
         )
 
-        print("ln =", ln)
-
         value_arccos = np.dot(im, ln) / np.dot(np.linalg.norm(im), np.linalg.norm(ln))
-        print(value_arccos)
-        # pass
+
         sign_angle = np.sign(np.dot(vector_ij, np.cross(vector_kj, vector_kl)))
-        print(sign_angle)
-        angle = math.degrees(np.arccos(value_arccos) * sign_angle)
-        print(angle)
+
+        angle_psi = math.degrees(np.arccos(value_arccos) * sign_angle)
+        list_angle_psi.append(angle_psi)
+
+    for vector in vector_phi:
+        vector_ij_phi = np.array(vector[0])
+        vector_kj_phi = np.array(vector[1])
+        vector_kl_phi = np.array(vector[2])
+
+        im_phi = vector_ij_phi - np.dot(
+            (np.dot(vector_ij_phi, vector_kj_phi) / np.linalg.norm(vector_kj_phi)),
+            vector_kj_phi,
+        )
+
+        ln_phi = -vector_kl_phi + np.dot(
+            (np.dot(vector_kl_phi, vector_kj_phi) / np.linalg.norm(vector_kj_phi)),
+            vector_kj_phi,
+        )
+
+        value_arccos_phi = np.dot(im_phi, ln_phi) / np.dot(
+            np.linalg.norm(im_phi), np.linalg.norm(ln_phi)
+        )
+
+        sign_angle_phi = np.sign(
+            np.dot(vector_ij_phi, np.cross(vector_kj_phi, vector_kl_phi))
+        )
+
+        angle_phi = math.degrees(np.arccos(value_arccos_phi) * sign_angle_phi)
+        list_angle_phi.append(angle_phi)
+    return list_angle_psi, list_angle_phi
 
 
 """Main function."""
@@ -296,17 +302,20 @@ def Molecular_Dynamics_Simulation(file_input, file_data):
 
             from_index = 0
             to_index = int(number_atoms) + 2
+            # calculate number of psi angle and phi angle
             list_angle_psi, list_angle_phi = count_angle(file_input, number_atoms)
             number_angle_psi = len(list_angle_psi)
             number_angle_phi = len(list_angle_phi)
 
+            # initialize size of matrix
             cols = 2 + number_angle_phi + number_angle_psi
-            rows = 2
+            rows = 5
 
             # matrix_data = np.tile(np.arange(rows), (columns, 1))
             matrix_data = [[0 for i in range(cols)] for j in range(rows)]
             index_psi = 1 + number_angle_psi
 
+            # first row of matrix with title
             for i in range(len(matrix_data[0])):
                 if i == 0:
                     matrix_data[0][0] = "t"
@@ -319,7 +328,7 @@ def Molecular_Dynamics_Simulation(file_input, file_data):
 
             # for row in matrix_data:
             #     print(row)
-            print(matrix_data)
+            # print(matrix_data)
             # for i in range(0, int(number_frames)):
             for i in range(0, 2):
                 print(from_index)
@@ -328,12 +337,37 @@ def Molecular_Dynamics_Simulation(file_input, file_data):
                 t, distance = count_distance(
                     file_input, number_atoms, from_index, to_index
                 )
-                print(t, distance)
+                # write t to matrix
+                matrix_data[i + 1][0] = t
+                # write distance to matrix
+                matrix_data[i + 1][1] = distance
+                # del list_angle_psi[:]
+                # del list_angle_phi[:]
+                # print("angle_psi avant", angle_psi)
+                # print("angle_phi apres", angle_phi)
+                angle_psi, angle_phi = dihedral_angle(
+                    file_input,
+                    number_atoms,
+                    list_angle_psi,
+                    list_angle_phi,
+                    from_index,
+                    to_index,
+                )
+
+                for j in range(len(matrix_data[0]) - 2):
+                    print(j)
+                    if j < len(angle_psi):
+                        matrix_data[i + 1][j + 2] = angle_psi[j]
+                    else:
+                        matrix_data[i + 1][j + 2] = angle_phi[j - 4]
+
+                # print("angle_psi", angle_psi)
+                # print("angle_phi", angle_phi)
 
                 from_index = from_index + 3 + int(number_atoms)
                 to_index += int(number_atoms) + 3
-            # dihedral_angle(file_input, number_atoms)
 
+            print(matrix_data)
     except Exception as e:
         logging.info(f"Error while opening file gro: {e}")
 

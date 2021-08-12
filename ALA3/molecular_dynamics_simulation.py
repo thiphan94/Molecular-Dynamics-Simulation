8b368d12304fd6a43f99cb5b23fc1706d79d680b
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2021-08-12 19:38:59 +0200

    Optimize

--- ALA3/molecular_dynamics_simulation.py
+++ ALA3/molecular_dynamics_simulation.py
@@ -7,141 +7,126 @@ import re
 import numpy as np
 import sys
 import pandas as pd
-
+import time
 
 """Function to calculate frames in file."""
 
 
-def count_frame(file):
-    with open(file) as f:
-        count = 0
-        for line in f:
-            # line starts with "G"
-            if line.startswith("G"):
-                count += 1
-        return count
+def count_frame(lines):
+    count = 0
+    for line in lines:
+        # line starts with "G"
+        if line.startswith("G"):
+            count += 1
+    return count
 
 
 """Function to calculate the end-to-end distance."""
 
 
-def count_distance(file, number, from_index, to_index):
-    with open(file) as f:
-        lines = f.readlines()
-        lines = lines[from_index:to_index]
-        for index, line in enumerate(lines):
+def count_distance(lines, number, from_index, to_index):
+    sublines = lines[from_index:to_index]
+    for index, line in enumerate(sublines):
 
-            pattern = "=(.+?)step"
-            if line == number:
-                t = re.search("=(.+?)step", lines[index - 1]).group(1)
+        pattern = "=(.+?)step"
+        if line == number:
+            t = re.search("=(.+?)step", sublines[index - 1]).group(1)
 
-                atom1 = lines[index + 1]
-                atom2 = lines[index + int(number)]
-                list_atom1 = atom1.split()
-                list_atom2 = atom2.split()
-                list_atom1 = list(map(float, list_atom1[3:6]))
-                list_atom2 = list(map(float, list_atom2[3:6]))
-                distance = (
-                    ((list_atom2[0] - list_atom1[0]) ** 2)
-                    + ((list_atom2[1] - list_atom1[1]) ** 2)
-                    + ((list_atom2[2] - list_atom1[2]) ** 2)
-                ) ** 0.5
+            atom1 = sublines[index + 1]
+            atom2 = sublines[index + int(number)]
+            list_atom1 = atom1.split()
+            list_atom2 = atom2.split()
+            list_atom1 = list(map(float, list_atom1[3:6]))
+            list_atom2 = list(map(float, list_atom2[3:6]))
+            distance = (
+                ((list_atom2[0] - list_atom1[0]) ** 2)
+                + ((list_atom2[1] - list_atom1[1]) ** 2)
+                + ((list_atom2[2] - list_atom1[2]) ** 2)
+            ) ** 0.5
 
-        return float(t), distance
+    return float(t), distance
 
 
-def count_angle(file, number):
-    with open(file) as f:
-        last = int(number) + 2
-        lines = f.readlines()
-        # list of atoms to calculate angle phi
-        list_angle_phi = []
-        # list of atoms to calculate angle psi
-        list_angle_psi = []
+def count_angle(lines, number):
+    last = int(number) + 2
+    # list of atoms to calculate angle phi
+    list_angle_phi = []
+    # list of atoms to calculate angle psi
+    list_angle_psi = []
 
-        # index of four atoms in chain phi
-        phi = []
-        # index of four atoms in chain psi
-        psi = []
+    # index of four atoms in chain phi
+    phi = []
+    # index of four atoms in chain psi
+    psi = []
 
-        for index, line in enumerate(lines[0:last]):
-            if len(lines[index].split()) > 4 and lines[index].split()[1] != "by":
-                if lines[index].split()[1] == "N":
-                    psi.append(lines[index].split()[2])
+    for index, line in enumerate(lines[0:last]):
+        if len(lines[index].split()) > 4 and lines[index].split()[1] != "by":
+            if lines[index].split()[1] == "N":
+                psi.append(lines[index].split()[2])
 
-                    if len(phi) == 1:
-                        phi.append(lines[index].split()[2])
+                if len(phi) == 1:
+                    phi.append(lines[index].split()[2])
 
-                if lines[index].split()[1] == "CA":
-                    psi.append(lines[index].split()[2])
+            if lines[index].split()[1] == "CA":
+                psi.append(lines[index].split()[2])
 
-                    if len(phi) == 2:
-                        phi.append(lines[index].split()[2])
+                if len(phi) == 2:
+                    phi.append(lines[index].split()[2])
 
-                if lines[index].split()[1] == "C":
-                    psi.append(lines[index].split()[2])
+            if lines[index].split()[1] == "C":
+                psi.append(lines[index].split()[2])
 
-                    if len(phi) == 0 or len(psi) == 3:
-                        phi.append(lines[index].split()[2])
+                if len(phi) == 0 or len(psi) == 3:
+                    phi.append(lines[index].split()[2])
 
-                if len(phi) == 4:
-                    list_angle_phi.append(phi[:])
-                    del phi[:3]
-                if len(psi) == 4:
-                    list_angle_psi.append(psi[:])
-                    del psi[:3]
+            if len(phi) == 4:
+                list_angle_phi.append(phi[:])
+                del phi[:3]
+            if len(psi) == 4:
+                list_angle_psi.append(psi[:])
+                del psi[:3]
 
-        return (list_angle_psi, list_angle_phi)
+    return (list_angle_psi, list_angle_phi)
 
 
-"""Function to calculate dihedral angles."""
+def dihedral_angle(lines, list_angle_psi, list_angle_phi, from_index, to_index):
+    """Function to calculate dihedral angles."""
+    sublines = lines[from_index:to_index]
 
+    list_coordinates_psi = []
+    list_coordinates_phi = []
+    phi_coordinates = []
+    psi_coordinates = []
+    vector_psi = []
+    vector_phi = []
 
-def dihedral_angle(file, number, list_angle_psi, list_angle_phi, from_index, to_index):
-    with open(file) as f:
-        lines = f.readlines()
-        lines = lines[from_index:to_index]
+    for index, line in enumerate(sublines):
+        if len(sublines[index].split()) > 4 and sublines[index].split()[1] != "by":
+            if any(sublines[index].split()[2] in sublist for sublist in list_angle_psi):
 
-        list_coordinates_psi = []
-        list_coordinates_phi = []
-        phi_coordinates = []
-        psi_coordinates = []
-        vector_psi = []
-        vector_phi = []
+                psi_coordinates.append(sublines[index].split()[3:6])
 
-        for index, line in enumerate(lines):
-            if len(lines[index].split()) > 4 and lines[index].split()[1] != "by":
-                if any(
-                    lines[index].split()[2] in sublist for sublist in list_angle_psi
-                ):
+            if any(sublines[index].split()[2] in sublist for sublist in list_angle_phi):
 
-                    psi_coordinates.append(lines[index].split()[3:6])
+                phi_coordinates.append(sublines[index].split()[3:6])
 
-                if any(
-                    lines[index].split()[2] in sublist for sublist in list_angle_phi
-                ):
+            if len(phi_coordinates) == 4:
+                list_coordinates_phi.append(phi_coordinates[:])
+                del phi_coordinates[:3]
 
-                    phi_coordinates.append(lines[index].split()[3:6])
+            if len(psi_coordinates) == 4:
+                list_coordinates_psi.append(psi_coordinates[:])
+                del psi_coordinates[:3]
 
-                if len(phi_coordinates) == 4:
-                    list_coordinates_phi.append(phi_coordinates[:])
-                    del phi_coordinates[:3]
+    vector_psi, vector_phi = vector(list_coordinates_psi, list_coordinates_phi)
 
-                if len(psi_coordinates) == 4:
-                    list_coordinates_psi.append(psi_coordinates[:])
-                    del psi_coordinates[:3]
+    angle_psi, angle_phi = value_angle(vector_psi, vector_phi)
 
-        vector_psi, vector_phi = vector(list_coordinates_psi, list_coordinates_phi)
-
-        angle_psi, angle_phi = value_angle(vector_psi, vector_phi)
-
-        return angle_psi, angle_phi
-
-
-"""Function to calculate vector between 4 atoms."""
+    return angle_psi, angle_phi
 
 
 def vector(list_coordinates_psi, list_coordinates_phi):
+    """Function to calculate vector between 4 atoms."""
     ij_psi = []
     kj_psi = []
     kl_psi = []
@@ -273,79 +258,77 @@ def value_angle(vector_psi, vector_phi):
 def Molecular_Dynamics_Simulation(file_input, file_data):
     try:
         with open(file_input) as f:
-            number_frames = count_frame(file_input)
-            print("Number of frames:", number_frames)
-            number_atoms = linecache.getline(file_input, 2)
-            print("Number of atoms:", number_atoms)
-
-            from_index = 0
-            to_index = int(number_atoms) + 2
-            # calculate number of psi angle and phi angle
-            list_angle_psi, list_angle_phi = count_angle(file_input, number_atoms)
-            number_angle_psi = len(list_angle_psi)
-            number_angle_phi = len(list_angle_phi)
-
-            # initialize size of matrix
-            cols = 2 + number_angle_phi + number_angle_psi
-
-            rows = number_frames
-
-            values = [[0 for i in range(cols)] for j in range(rows)]
-            index_psi = 1 + number_angle_psi
-
-            columns = []
-            index_psi = 0
-            index_phi = 1
-
-            for i in range(0, cols):
-                if i == 0:
-                    columns.append("t")
-                elif i == 1:
-                    columns.append("distance")
-                elif i % 2 == 0 and i != 0:
-                    columns.append("psi" + str(index_psi))
-                    index_psi += 1
-                else:
-                    columns.append("phi" + str(index_phi))
-                    index_phi += 1
-
-            # for i in range(0, 100):
-            for i in range(0, number_frames):
-                print(i)
-                t, distance = count_distance(
-                    file_input, number_atoms, from_index, to_index
-                )
-                # write t to matrix
-                values[i][0] = t
-                # write distance to matrix
-                values[i][1] = distance
-
-                angle_psi, angle_phi = dihedral_angle(
-                    file_input,
-                    number_atoms,
-                    list_angle_psi,
-                    list_angle_phi,
-                    from_index,
-                    to_index,
-                )
-
-                for j in range(len(values[0]) - 2):
-                    if j < len(angle_psi):
-                        values[i][j + 2] = angle_psi[j]
-                    else:
-                        values[i][j + 2] = angle_phi[j - 4]
-
-                from_index = from_index + 3 + int(number_atoms)
-                to_index += int(number_atoms) + 3
-
-            df = pd.DataFrame(values, columns=columns)
-
-            df.to_csv(file_data, index=False)
-
+            lines = f.readlines()
     except Exception as e:
         logging.info(f"Error while opening file gro: {e}")
 
+    number_frames = count_frame(lines)
+    print("Number of frames:", number_frames)
+    number_atoms = lines[1]
+    print("Number of atoms:", number_atoms)
+    from_index = 0
+    to_index = int(number_atoms) + 2
+    # calculate number of psi angle and phi angle
+    list_angle_psi, list_angle_phi = count_angle(lines, number_atoms)
+    number_angle_psi = len(list_angle_psi)
+    number_angle_phi = len(list_angle_phi)
 
+    # initialize size of matrix
+    cols = 2 + number_angle_phi + number_angle_psi
+
+    rows = number_frames
+
+    values = [[0 for i in range(cols)] for j in range(rows)]
+    index_psi = 1 + number_angle_psi
+
+    columns = []
+    index_psi = 0
+    index_phi = 1
+
+    for i in range(0, cols):
+        if i == 0:
+            columns.append("t")
+        elif i == 1:
+            columns.append("distance")
+        elif i % 2 == 0 and i != 0:
+            columns.append("psi" + str(index_psi))
+            index_psi += 1
+        else:
+            columns.append("phi" + str(index_phi))
+            index_phi += 1
+
+    # for i in range(0, 100):
+    for i in range(0, number_frames):
+        t, distance = count_distance(lines, number_atoms, from_index, to_index)
+        # write t to matrix
+        values[i][0] = t
+        # write distance to matrix
+        values[i][1] = distance
+
+        angle_psi, angle_phi = dihedral_angle(
+            lines,
+            list_angle_psi,
+            list_angle_phi,
+            from_index,
+            to_index,
+        )
+
+        for j in range(len(values[0]) - 2):
+            if j < len(angle_psi):
+                values[i][j + 2] = angle_psi[j]
+            else:
+                values[i][j + 2] = angle_phi[j - 4]
+
+        from_index = from_index + 3 + int(number_atoms)
+        to_index += int(number_atoms) + 3
+
+    df = pd.DataFrame(values, columns=columns)
+
+    df.to_csv(file_data, index=False)
+
+
+start_time = time.time()
 file_input = str(sys.argv[1])
 file_data = str(sys.argv[2])
 Molecular_Dynamics_Simulation(file_input, file_data)
+print("--- %s seconds ---" % (time.time() - start_time))
